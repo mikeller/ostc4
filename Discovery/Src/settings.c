b186960ad505325040bf161351ea64b942f5b25e
Michael Keller <github@ike.ch>
2021-07-27 21:29:24 +1200

    Updated version.

--- Discovery/Src/settings.c
+++ Discovery/Src/settings.c
@@ -66,11 +66,11 @@ const SFirmwareData firmware_FirmwareData __attribute__( (section(".firmware_fir
 {
     .versionFirst   = 1,
     .versionSecond 	= 7,
-    .versionThird   = 2,
-    .versionBeta    = 0,
+    .versionThird   = 1,
+    .versionBeta    = 1,
 
     /* 4 bytes with trailing 0 */
-    .signature = "mh",
+    .signature = "mk",
 
     .release_year = 25,
     .release_month = 4,
