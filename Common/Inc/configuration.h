48a8320ecc4ed2ba8465e0ad666c0b004d2f7685
Michael Keller <github@ike.ch>
2023-02-14 16:13:41 +1300

    Updated packaging script, added eclipse build for it.
    Fix building of firmware that does not contain all parts of (firmware, fonts, RTE).
    
    Added the option to only build parts to the un*x build script.
    
    Added external builder for Firmware and RTE.
    
    Fix RTE packaging.
    
    Added builder for fonts, updated packaging script to build Release (required for fonts).
    
    Added builders for packaging.

--- Common/Inc/configuration.h
+++ Common/Inc/configuration.h
@@ -46,7 +46,7 @@
 /* #define ENABLE_ANALYSE_SAMPLES */
 
 /* Enable to have access to the debug view options (turn on / off via menu instead of compile switch) */
-/* #define HAVE_DEBUG_VIEW */
+#define HAVE_DEBUG_VIEW
 
 /* Enable to have access to the motion control selection menu */
 /* #define ENABLE_MOTION_CONTROL */
