f744caa7fa2b661ac6895c732e917f998e8b9428
Michael Keller <github@ike.ch>
2025-09-04 11:38:56 +1200

    Update the arm build tooling.

--- arm_build_tools.mk
+++ arm_build_tools.mk
@@ -1,7 +1,30 @@
+#
+# Download URL: https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
+#
+
 ARM_TOOLCHAIN_VERSION ?= 9-2020-q2-update
-ARM_TOOLCHAIN_OS ?= x86_64-linux
+ARM_TOOLCHAIN_PREFIX ?= gcc-arm-none-eabi
+
+ARM_TOOLCHAIN_OS ?=
+ifeq ($(strip $(ARM_TOOLCHAIN_OS)),)
+  UNAME_S := $(shell uname -s 2>/dev/null)
+  ifeq ($(UNAME_S),Linux)
+    ARM_TOOLCHAIN_OS := x86_64-linux
+  else ifeq ($(UNAME_S),Darwin)
+    ARM_TOOLCHAIN_OS := mac
+  else ifneq ($(filter MSYS_NT-% MINGW%,$(UNAME_S)),)
+    ARM_TOOLCHAIN_OS := win32
+  else ifdef OS
+  # Basic Windows detection
+    ARM_TOOLCHAIN_OS := win32
+  else
+    ARM_TOOLCHAIN_OS := x86_64-linux
+  endif
+endif
+
+ARM_TOOLCHAIN := $(ARM_TOOLCHAIN_PREFIX)-$(ARM_TOOLCHAIN_VERSION)-$(ARM_TOOLCHAIN_OS)
+ARM_TOOLCHAIN_ARCHIVE := $(ARM_TOOLCHAIN).tar.bz2
 ARM_TOOLCHAIN_URL ?= https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/$(ARM_TOOLCHAIN_ARCHIVE)
-ARM_TOOLCHAIN_SHA256 ?=
 
 ifndef BUILD_TOOLS_DIR
 ifndef BUILD_DIR
@@ -11,34 +34,29 @@ endif
 BUILD_TOOLS_DIR := $(BUILD_DIR)/tools
 endif
 
-ARM_TOOLCHAIN_ARCHIVE := gcc-arm-none-eabi-$(ARM_TOOLCHAIN_VERSION)-$(ARM_TOOLCHAIN_OS).tar.bz2
-TOOLCHAIN_DIR := $(BUILD_TOOLS_DIR)/gcc-arm-none-eabi-$(ARM_TOOLCHAIN_VERSION)
-TOOLCHAIN_LINK := $(BUILD_TOOLS_DIR)/gcc-arm-none-eabi
-export PATH := $(abspath $(TOOLCHAIN_LINK)/bin):$(PATH)
+TOOLCHAIN_DIR := $(BUILD_TOOLS_DIR)/$(ARM_TOOLCHAIN_PREFIX)-$(ARM_TOOLCHAIN_VERSION)
+export PATH := $(abspath $(TOOLCHAIN_DIR)/bin):$(PATH)
 
-arm_tools: $(TOOLCHAIN_LINK)
+arm_tools_install: $(TOOLCHAIN_DIR)
 
-$(TOOLCHAIN_LINK):
-	$(MAKE) arm_tools_clean
+$(TOOLCHAIN_DIR):
+	$(MAKE) arm_tools_uninstall
 	@set -eu; \
 	mkdir -p "$(BUILD_TOOLS_DIR)/dist"; \
 	if [ ! -d "$(TOOLCHAIN_DIR)" ]; then \
 	  echo "Downloading $(ARM_TOOLCHAIN_URL)"; \
 	  curl -fL --retry 3 -z "$(BUILD_TOOLS_DIR)/dist/$(ARM_TOOLCHAIN_ARCHIVE)" -o "$(BUILD_TOOLS_DIR)/dist/$(ARM_TOOLCHAIN_ARCHIVE)" "$(ARM_TOOLCHAIN_URL)"; \
-	  if [ -n "$(ARM_TOOLCHAIN_SHA256)" ]; then \
-	    echo "$(ARM_TOOLCHAIN_SHA256)  $(BUILD_TOOLS_DIR)/dist/$(ARM_TOOLCHAIN_ARCHIVE)" | sha256sum -c -; \
-	  fi; \
 	  echo "Unpacking $(ARM_TOOLCHAIN_ARCHIVE)"; \
 	  tar -xjf "$(BUILD_TOOLS_DIR)/dist/$(ARM_TOOLCHAIN_ARCHIVE)" -C "$(BUILD_TOOLS_DIR)"; \
 	fi; \
-	ln -sfn "$(TOOLCHAIN_DIR)" "$(TOOLCHAIN_LINK)"
 
 arm_tools_version:
 	@echo $(ARM_TOOLCHAIN_VERSION)
 
-arm_tools_clean:
-	rm -rf "$(TOOLCHAIN_LINK)" "$(TOOLCHAIN_DIR)"
+arm_tools_uninstall:
+	$(RM) -rf "$(TOOLCHAIN_DIR)"
 
-distclean: arm_tools_clean
+distclean: arm_tools_uninstall
+	$(RM) -rf "$(BUILD_TOOLS_DIR)/dist"
 
-.PHONY: arm_tools arm_tools_version arm_tools_clean distclean
+.PHONY: arm_tools_install arm_tools_version arm_tools_uninstall distclean
