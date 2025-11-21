8837d3a4fc39fb3fc978fec8bcd6c1345d6b334e
Michael Keller <github@ike.ch>
2025-11-22 12:48:28 +1300

    Make the build actually work.
    Signed-off-by: Michael Keller <github@ike.ch>

--- Discovery/Src/firmwareEraseProgram.c
+++
@@ -1,1 +0,0 @@
-../../OtherSources/firmwareEraseProgram.c
---
+++ RefPrj/BootLoader/Release/Discovery/Src/subdir.mk
@@ -0,0 +1,58 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+$(BUILD_DIR)/Discovery/Src/startup_stm32f429xx.s 
+
+C_SRCS += \
+$(BUILD_DIR)/Discovery/Src/crcmodel.c \
+$(BUILD_DIR)/Discovery/Src/firmwareJumpToApplication.c \
+$(BUILD_DIR)/Discovery/Src/gfx_colors.c
+
+OBJS += \
+./Discovery/Src/crcmodel.o \
+./Discovery/Src/firmwareJumpToApplication.o \
+./Discovery/Src/gfx_colors.o \
+./Discovery/Src/startup_stm32f429xx.o \
+
+C_DEPS += \
+./Discovery/Src/crcmodel.d \
+./Discovery/Src/firmwareJumpToApplication.d \
+./Discovery/Src/gfx_colors.d \
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Discovery/Src/crcmodel.o: $(BUILD_DIR)/Discovery/Src/crcmodel.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DSTM32F4 -DSTM32F429xx -DSTM32F429IITx -I"$(BUILD_DIR)/Discovery/Inc" -I"$(BUILD_DIR)/Common/Drivers/CMSIS/Include" -I"$(BUILD_DIR)/Common/Drivers/STM32F4xx/Include" -I"$(BUILD_DIR)/Common/Drivers/STM32F4xx_HAL_Driver/Inc" -I"$(BUILD_DIR)/Common/Inc" -O2 -Wall -ffunction-sections -fdata-sections -fmessage-length=0 -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Discovery/Src/firmwareJumpToApplication.o: $(BUILD_DIR)/Discovery/Src/firmwareJumpToApplication.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DSTM32F4 -DSTM32F429xx -DSTM32F429IITx -I"$(BUILD_DIR)/Discovery/Inc" -I"$(BUILD_DIR)/Common/Drivers/CMSIS/Include" -I"$(BUILD_DIR)/Common/Drivers/STM32F4xx/Include" -I"$(BUILD_DIR)/Common/Drivers/STM32F4xx_HAL_Driver/Inc" -I"$(BUILD_DIR)/Common/Inc" -O2 -Wall -ffunction-sections -fdata-sections -fmessage-length=0 -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Discovery/Src/gfx_colors.o: $(BUILD_DIR)/Discovery/Src/gfx_colors.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DSTM32F4 -DSTM32F429xx -DSTM32F429IITx -I"$(BUILD_DIR)/Discovery/Inc" -I"$(BUILD_DIR)/Common/Drivers/CMSIS/Include" -I"$(BUILD_DIR)/Common/Drivers/STM32F4xx/Include" -I"$(BUILD_DIR)/Common/Drivers/STM32F4xx_HAL_Driver/Inc" -I"$(BUILD_DIR)/Common/Inc" -O2 -Wall -ffunction-sections -fdata-sections -fmessage-length=0 -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Discovery/Src/startup_stm32f429xx.o: $(BUILD_DIR)/Discovery/Src/startup_stm32f429xx.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Assembler'
+	@echo $(PWD)
+	arm-none-eabi-as -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
