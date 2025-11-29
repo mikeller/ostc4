7d86358555467aafde2b4b3a6e840e157c3ceaba
Michael Keller <github@ike.ch>
2025-11-29 19:54:15 +1300

    Make the bootloader work for OSTC4 and OSTC5.
    Signed-off-by: Michael Keller <github@ike.ch>

--- RefPrj/BootLoader/Release/Discovery/Src/subdir.mk
+++ RefPrj/BootLoader/Release/Discovery/Src/subdir.mk
@@ -9,18 +9,21 @@ $(BUILD_DIR)/Discovery/Src/startup_stm32f429xx.s
 C_SRCS += \
 $(BUILD_DIR)/Discovery/Src/crcmodel.c \
 $(BUILD_DIR)/Discovery/Src/firmwareJumpToApplication.c \
-$(BUILD_DIR)/Discovery/Src/gfx_colors.c
+$(BUILD_DIR)/Discovery/Src/gfx_colors.c \
+$(BUILD_DIR)/Discovery/Src/stm32f4xx_it.c
 
 OBJS += \
 ./Discovery/Src/crcmodel.o \
 ./Discovery/Src/firmwareJumpToApplication.o \
 ./Discovery/Src/gfx_colors.o \
 ./Discovery/Src/startup_stm32f429xx.o \
+./Discovery/Src/stm32f4xx_it.o
 
 C_DEPS += \
 ./Discovery/Src/crcmodel.d \
 ./Discovery/Src/firmwareJumpToApplication.d \
 ./Discovery/Src/gfx_colors.d \
+./Discovery/Src/stm32f4xx_it.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -48,6 +51,14 @@ Discovery/Src/gfx_colors.o: $(BUILD_DIR)/Discovery/Src/gfx_colors.c
 	@echo 'Finished building: $<'
 	@echo ' '
 
+Discovery/Src/stm32f4xx_it.o: $(BUILD_DIR)/Discovery/Src/stm32f4xx_it.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DSTM32F4 -DSTM32F429xx -DSTM32F429IITx -I"$(BUILD_DIR)/Discovery/Inc" -I"$(BUILD_DIR)/Common/Drivers/CMSIS/Include" -I"$(BUILD_DIR)/Common/Drivers/STM32F4xx/Include" -I"$(BUILD_DIR)/Common/Drivers/STM32F4xx_HAL_Driver/Inc" -I"$(BUILD_DIR)/Common/Inc" -O2 -Wall -ffunction-sections -fmessage-length=0 -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 Discovery/Src/startup_stm32f429xx.o: $(BUILD_DIR)/Discovery/Src/startup_stm32f429xx.s
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Assembler'
@@ -55,4 +66,3 @@ Discovery/Src/startup_stm32f429xx.o: $(BUILD_DIR)/Discovery/Src/startup_stm32f42
 	arm-none-eabi-as -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
-
